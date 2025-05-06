5f1a35f8f0ad981a21a4fd031e04b83e3efd5f47
Patelpankaj1947 <patep9717@gmail.com>
2025-05-06 10:39:22 +0530

    Initial commit

---
+++ src/data.java
@@ -0,0 +1,20 @@
+public class data {
+    public static void main(String[] args) {
+      byte a = 22;
+      short b = 12;
+      long c = 36;
+      int d = 20;
+      float e = 2.2f;
+      double f= 23.333333;
+      char g= 'e';
+      boolean h= true ;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println( f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
---
+++ src/pkj.java
@@ -0,0 +1,7 @@
+ public class pkj {
+    public static void main(String[] args) {
+        int[] arr = {3,0,1};
+       // int [] sum = arr[index++];
+        System.out.println();
+    }
+}
